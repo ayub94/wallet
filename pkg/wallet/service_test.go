137fb608c8831d1caed5391105bd7644ac2048cc
Ayub <ayubjon94.an@gmail.com>
2020-10-04 13:02:16 +0500

    added testService_Reject

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -33,3 +33,24 @@ func TestService_FindAccountByID(t *testing.T) {
 		})
 	}
 }
+
+func TestService_Reject(t *testing.T) {
+	type args struct {
+		paymentID string
+	}
+	tests := []struct {
+		name    string
+		s       *Service
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.s.Reject(tt.args.paymentID); (err != nil) != tt.wantErr {
+				t.Errorf("Service.Reject() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
