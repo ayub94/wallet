820558057a1bf71a87bbec00d7e10875c5033d93
Ayub <ayubjon94.an@gmail.com>
2020-10-11 23:55:16 +0500

    new eddition1

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -193,7 +193,7 @@ func (s *Service)PayFromFavorite(favoriteID string)(*types.Payment, error) {
 }
 
 func (s *Service)ExportToFile(path string) error {
-	file, err := os.OpenFile(path, os.O_CREATE | os.O_WRONLY | os.O_APPEND, 2048)
+	file, err := os.Open(path)    //File(path, os.O_CREATE | os.O_WRONLY | os.O_APPEND, 2048)
 	if err != nil {
 		log.Print(err)
 		return ErrFileNotFound
